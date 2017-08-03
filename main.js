58d8a1e9cd27c391db75bdcca2b897a7b13ded73
David Brear <davidbrear04@gmail.com>
2017-08-03 15:31:17 -0400

    got file dialog selecting md files

--- main.js
+++ main.js
@@ -59,7 +59,12 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-exports.selectResultsDirectory = function(cb) {
+exports.selectResultsDirectory = function(markDownString) {
   dialog.showSaveDialog(mainWindow, {
-  }, cb);
+    filters: [
+      {name: 'Markdown Files', extensions: ['md']},
+    ]
+  }, (directory) => {
+    
+  });
 }
